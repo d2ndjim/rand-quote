4cbc17cf940f78b9b2da909f3067f24999ff1b10
Lekan <jimoridwanlekan@gmail.com>
2022-12-15 10:41:05 +0100

    fetch data to populate db from Api with rest client

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'rest-client'
+
+fetched_data = RestClient::Request.execute(method: :get,
+                                       url: "https://type.fit/api/quotes")
+                                       
+data = JSON.parse(fetched_data)
+data.each do |quote|
+  Quote.create(text: quote[:text], author: quote[:author])
+end
