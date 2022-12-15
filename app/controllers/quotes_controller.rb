1e76911bcd48df2f099a6c4c5d79d6724a4701f8
Lekan <jimoridwanlekan@gmail.com>
2022-12-15 11:23:11 +0100

    Fix linters errors

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,14 +1,15 @@
 class QuotesController < ApplicationController
   def index
-    @quote = Quote.order("RANDOM()").first
+    @quote = Quote.order('RANDOM()').first
   end
 
   def create
     @quote = Quote.create(quote_params)
-	  redirect_to root_path
+    redirect_to root_path
   end
 
   private
+
   def quote_params
     params.require(:quote).permit(:text, :author)
   end
--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Quote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Quote.new(text: 'There are no failures just experiences and your reactions to them.', author: 'Tom Krause')
+  end
+  before { subject.save }
+
+  it 'text should not be nil' do
+    subject.text = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should valid the author' do
+    subject.author = 'Tom Krause'
+    expect(subject).to be_valid
+  end
 end
